efe5383ecdb2690adf6a2814d79268ea516a315d
PedroAugusto2101 <pedrotiagobh@gmail.com>
2024-05-13 21:36:18 -0300

    20240513-01-criacao-arquivo-digital-clock

---
+++ digital-clock/assets/js/script.js
@@ -0,0 +1,12 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    hours.textContent = hr;
+})
