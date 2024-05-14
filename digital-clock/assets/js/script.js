44a4767c7da1e7f3ebfee0d6d5d813795d990d61
PedroAugusto2101 <pedrotiagobh@gmail.com>
2024-05-14 20:08:29 -0300

    20240514-02-finalizacao-projeto-digital-clock

--- digital-clock/assets/js/script.js
+++ digital-clock/assets/js/script.js
@@ -8,5 +8,14 @@ const clock = setInterval(function time() {
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
     hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = s;
 })
